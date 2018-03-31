3f00cfcf377a247bc4792ba2c80b9aa1890cef4b
JunchunDai <daijunchun2675@126.com>
2018-03-31 20:15:12 +0800

    怎么提交不了了?'

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('#content').fullpage({
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#ccc','#fff'],
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5','page6'],
+    menu: '#menu'
+  })
+})
